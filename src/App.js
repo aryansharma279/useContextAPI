09053ac6e75556d938ce7622c0ca5696b0e44a15
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-19 00:34:34 +0530

    [NEW] Passing multiple data with use context api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import { createContext , useState, Provider } from 'react';
-import ComA from './components/comA/ComA';
+import "./App.css";
+import { createContext, useState } from "react";
+import ComA from "./components/comA/ComA";
 
 //declared outside the function component as individual
 const AppDataContext = createContext();
+const SomeExtra = createContext();
 
 function App() {
-  const [data,setData] = useState("aryan");
+  const [data, setData] = useState("aryan");
+  const [newData, setNewData] = useState({ name: "Inu", lastName: "sharma" });
+  const [extra, setExtra] = useState({ little: "something Good" });
   return (
-    <AppDataContext.Provider value={data}>
-    <div className="App">
-    
-      <h1>inside App </h1>
-      <ComA/>
-   
-    </div>
+    <AppDataContext.Provider value={{ data, newData }}>
+      <SomeExtra.Provider value={extra}>
+        <div className="App">
+          <h1>inside App </h1>
+          <ComA />
+        </div>
+      </SomeExtra.Provider>
     </AppDataContext.Provider>
   );
 }
 
 export default App;
-export {AppDataContext}
+export { AppDataContext, SomeExtra };
--- src/components/comA/comB/comC/ComC.js
+++ src/components/comA/comB/comC/ComC.js
@@ -1,17 +1,21 @@
-import React,{ useContext} from 'react'
-import { AppDataContext } from '../../../../App';
+import React, { useContext } from "react";
+import { AppDataContext } from "../../../../App";
+import { SomeExtra } from "../../../../App";
 import "./ComC.css";
 
 const ComC = () => {
   const AppData = useContext(AppDataContext);
-  console.log(AppDataContext);
+  const ExtraData = useContext(SomeExtra);
+  // console.log(AppDataContext);
   return (
-    <div className='backgroundc'>
+    <div className="backgroundc">
       <h1>inside C</h1>
-      <h2>{AppData}</h2>
-      
+      <h2>{AppData.data}</h2>
+      <h2>{AppData.newData.name}</h2>
+      <h2>{AppData.newData.lastName}</h2>
+      <h2>{ExtraData.little}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ComC
+export default ComC;
