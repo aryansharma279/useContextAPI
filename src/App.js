0bb8afa4b2ce059722c85d61a39f8568a40dcee6
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-27 13:22:51 +0530

    [NEW] useMemo Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import React from "react";
 import Com from "./components/Com";
-import Fetch from "./components/Fetch";
-//declared outside the function component as individual
 
 function App() {
-  const data = Fetch("https://hub.dummyapis.com/employee?noofRecords=2&idStarts=1001");
-  console.log(data);
   return (
-    
         <div className="App">
           <h1>inside App</h1>
           <Com/>
@@ -17,4 +12,3 @@ function App() {
 }
 
 export default App;
-
--- src/components/Com.js
+++ src/components/Com.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Fetch from './Fetch'
+import React, { useState, useMemo } from "react";
 
 const Com = () => {
+  const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
+
+  const longTime = (num) => {
+    for (let i = 0; i <= 1000000000000; i++) {
+      console.log("inside loop");
+      num = num + 1;
+      return num;
+    }
+  };
+  const calc = useMemo(() => {
+    longTime(count);
+  }, [count]);
+
+  // console.log("workin");
   return (
     <>
-    <div>Com</div>
-    <Fetch/>
-    </>
-  )
-}
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <h1>{count}</h1>
 
-export default Com
+        <input value={data} onChange={(e) => setData(e.target.value)} />
+        <h1>{data}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Com;
